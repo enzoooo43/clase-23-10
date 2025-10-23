f6b064527e96b9ce59a64f4b6b79ab3900b0bb5d
Enzo <erobles@colegioelbelloto.com>
2025-10-23 16:36:35 -0300

    se termina el formulario 2

--- biblioteca/src/main/java/cl/colegioelbelloto/biblioteca/controller/LibroController.java
+++ biblioteca/src/main/java/cl/colegioelbelloto/biblioteca/controller/LibroController.java
@@ -3,6 +3,7 @@ package cl.colegioelbelloto.biblioteca.controller;
 
 import cl.colegioelbelloto.biblioteca.model.Libro;
 
+import cl.colegioelbelloto.biblioteca.services.GoogleBooksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +12,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/libros")
 public class LibroController {
     @Autowired
     cl.colegioelbelloto.biblioteca.Service.LibroService libroService;
+    @Autowired
+    GoogleBooksService googleBooksService;
     @PostMapping("/guardar")
     public String guardarLibro(@ModelAttribute Libro libro, Model model){
         try {
+            googleBooksService.buscarPorIsbn(libro.getIsbn());
             libroService.saveLibro(libro);
             model.addAttribute("mensaje", "libro guardado exitosamente");
             model.addAttribute("libro", new Libro());
@@ -33,5 +39,11 @@ public class LibroController {
     public String mostrarFormulario(Model model){
         model.addAttribute("libro", new Libro());
         return "libro_form";
+    }
+    @GetMapping("/lista")
+    public String listarLibros(Model model){
+        List<Libro> libros = libroService.AllLibros();
+        model.addAttribute("libros", libros);
+        return "lista_libros";
     }
 }
